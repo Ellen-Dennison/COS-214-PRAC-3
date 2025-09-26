e10a0100508759a74995ac0f6f6b9b728993d535
NomziPhosa <157846833+NomziPhosa@users.noreply.github.com>
2025-09-26 20:02:55 +0200

    added users files

---
+++ Users.h
@@ -0,0 +1,34 @@
+#ifndef USERS_H
+#define USERS_H
+//#include "Command.h"
+#include "ChatRoom.h"
+#include<string>
+#include<vector>
+
+class ChatRoom;
+//class Command;
+
+class Users{
+    protected:
+              std::vector<ChatRoom*>* chatRooms; //pointer to vector of chatRoom pointers
+              std::string name;
+              //std::vector<Command*>* commandQueue;//pointer to vector of command pointers
+    public:
+             Users(const std::string& userName);
+             Users(const Users& other);
+             virtual ~Users();
+             const std::string& getName() const { return name; }
+             
+             // Make parameters const to match your implementation
+             void send(const std::string& message, ChatRoom* room);
+             void receive(const std::string& message, Users fromUser, ChatRoom* room);
+             //void addCommand(Command* command);
+             void executeAll();
+             void joinChatRoom(ChatRoom*);
+             void leaveChatRoom(ChatRoom*);
+             bool operator==(const Users& other) const;
+             const std::vector<ChatRoom*>& getChatRooms() const;
+             bool isInChatRoom(ChatRoom* room) const;
+             Users& operator=(const Users& other);
+};
+#endif
